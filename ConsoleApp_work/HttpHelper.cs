5bd891f490bd3f1c58b60d507a70218f59ce6654
Oleg Azemsha <bguproga@gmail.com>
2023-02-05 11:59:00 +0300

    .

---
+++ ConsoleApp_work/HttpHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_work
+{
+    public static class HttpHelper
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public static async void DownloadFileAsync(string uri, string outputPath)
+        {
+            Uri uriResult;
+
+            if (!Uri.TryCreate(uri, UriKind.Absolute, out uriResult))
+                throw new InvalidOperationException("URI is invalid.");
+
+            
+
+            var response = await _httpClient.GetAsync(uriResult);
+            var bt = await response.Content.ReadAsByteArrayAsync();
+            File.WriteAllBytes(outputPath, bt);
+            //using var writer = File.CreateText(outputPath);
+            //writer.Write(bt);
+            Console.WriteLine("OK");
+        }
+    }
+}
--- ConsoleApp_work/Program.cs
+++ ConsoleApp_work/Program.cs
@@ -3,27 +3,42 @@ using System.IO;
 using ConsoleApp_work;
 using System.Diagnostics;
 using BenchmarkDotNet.Running;
+using System.Net;
+using System.Net.NetworkInformation;
 
 #if DEBUG
-do
-{
-    Console.WriteLine("Enter the path file:");
-    string path = Console.ReadLine() ?? string.Empty;
-    path = "D:\\257.csv";
-    var t = new Stopwatch();
-    t.Start();
-    if (!File.Exists(path))
-    {
-        Console.WriteLine("This path is not exist");
-        continue;
-    }
-    var fileData = new FileData(path);
-    fileData.ReadAndWriteCsv();
-    t.Stop();
-    Console.WriteLine($"Read adn write finish t = {t.ElapsedMilliseconds}");
-    Console.WriteLine("---------------------------------------------");
+string url = "https://drive.google.com/u/0/uc?id=1nlcqoqpXKyfMs3nbBfhAGzniT4JXHu7Z&export=download";
+string save_path = "D:\\";
+string name = "popopop";
 
-} while (true);
+
+HttpHelper.DownloadFileAsync(url, save_path + name);
+Console.ReadKey();
+
+
+
+
+
+
+//do
+//{
+//    //Console.WriteLine("Enter the path file:");
+//    //string path = Console.ReadLine() ?? string.Empty;
+//    //path = "D:\\257.csv";
+//    //var t = new Stopwatch();
+//    //t.Start();
+//    //if (!File.Exists(path))
+//    //{
+//    //    Console.WriteLine("This path is not exist");
+//    //    continue;
+//    //}
+//    //var fileData = new FileData(path);
+//    //fileData.ReadAndWriteCsv();
+//    //t.Stop();
+//    //Console.WriteLine($"Read adn write finish t = {t.ElapsedMilliseconds}");
+//    //Console.WriteLine("---------------------------------------------");
+
+//} while (true);
 #else
 BenchmarkRunner.Run<DataBenchmarks>();
 #endif
